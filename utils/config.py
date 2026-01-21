3d9e46bfb2611bdf6887ca2d51cb98e112af3781
dim_stef <dimitriosstefanou97@gmail.com>
2026-01-21 17:05:11 +0200

    Update config.py
    Moved prompts and added new models.

--- utils/config.py
+++ utils/config.py
@@ -6,3 +6,23 @@ CORPUS_DIR = "data/corpus"
 MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 EMBED_MODEL = "sentence-transformers/all-MiniLM-L12-v2"
 RERANK_MODEL = "cross-encoder/ms-marco-MiniLM-L-6-v2"
+TOPIC_MODEL = "facebook/genre-kilt"
+
+# Prompts
+CHATBOT_SYSTEM = "You are a summariser. Answer using only the provided context. " \
+                "Do not repeat yourself, or the question, in your answer. " \
+                "Keep your sentences short, and your answer concise and complete." \
+                "if the context does not help, avoid answering. " \
+
+CHATBOT_PROMPT = """ \
+    <|system|>
+    {system}</s>
+
+    <|user|>
+    <context>
+    Provided context (might be cut-off/incomplete): {context}
+    </context>
+
+    Question: {input}</s>
+    <|assistant|>
+    """
